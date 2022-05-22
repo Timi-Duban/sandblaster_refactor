4d32878c36e32a47fdf1f5bf45fb4247784a9ac3
Timi-Duban <ti.mi.34@hotmail.fr>
2022-05-22 18:25:08 +0300

    add reverse_string test

--- tests/filters_test.py
+++ tests/filters_test.py
@@ -3,6 +3,7 @@
 import unittest
 from reverse_sandbox import filters
 
+
 class Initialize(unittest.TestCase):
     """Test the Filters object initialization
     """
@@ -12,7 +13,6 @@ class Initialize(unittest.TestCase):
         """
         filter_object = filters.Filters()
         self.assertIsNotNone(filter_object)
-
 
     def test_read(self):
         """Tests the "read_filters" function result
@@ -26,6 +26,7 @@ class Initialize(unittest.TestCase):
         self.assertIsNotNone(filter_object.filters_ios13)
         self.assertIsNotNone(filter_object.filters_ios14)
 
+
 class Methods(unittest.TestCase):
     """Tests the Filters methods
     """
@@ -35,7 +36,6 @@ class Methods(unittest.TestCase):
         filter_object = filters.Filters()
         self.assertEqual(filter_object.get_filters(9), filter_object.filters_ios11)
 
-
     def test_exists(self):
         """Tests the "exists" method
         """
@@ -43,13 +43,11 @@ class Methods(unittest.TestCase):
         self.assertTrue(filter_object.exists(5, 0x0d))
         self.assertFalse(filter_object.exists(11, 0xa8))
 
-
     def test_get(self):
         """Tests the "get" method
         """
         filter_object = filters.Filters()
         self.assertEqual(filter_object.get(14, 0x09), filter_object.filters_ios14.get(0x09))
-
 
 
 if __name__ == '__main__':
